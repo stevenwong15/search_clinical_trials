43dd301b219b4f49827f92e9b32a6fed841af8d2
stevenwong15 <stevenwong15@gmail.com>
2025-05-11 23:02:32 -0700

    added logic to trim text is exceeding allowed embedding tokens

--- utils.py
+++ utils.py
@@ -10,4 +10,8 @@ def get_token(text, model = "cl100k_base"):
     return tiktoken.get_encoding(model).encode(text)
 
 def get_embedding(text, model = "text-embedding-3-small"):
+    max_tokens = 8191
+    tokens = get_token(text)
+    if len(tokens) > max_tokens:
+        text = tiktoken.get_encoding("cl100k_base").decode(tokens[:max_tokens])
     return openai.embeddings.create(input = [text], model = model).data[0].embedding
